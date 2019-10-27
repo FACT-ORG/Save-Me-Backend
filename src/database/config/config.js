84a49d6622b30fdb665daad596d0af7fce089464
abejide001 <abejidefemi1@gmail.com>
2019-10-27 12:03:21 +0100

    fix deploy

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -13,7 +13,7 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    database_url: process.env.DATABASE_URL,
+    use_env_variable: process.env.DATABASE_URL,
     dialect: 'postgres',
     ssl: true,
     dialectOptions: {
